3984a770bc3588044c342c1ff4683718690734a9
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2022-11-14 22:59:03 -0300

    feat(header): implements header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,25 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './pages/login/login.component';
 import { ButtonComponent } from './components/button/button.component';
 import { FormComponent } from './components/form/form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigateComponent } from './components/navigate/navigate.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    AboutComponent,
     ButtonComponent,
     FormComponent,
     NavigateComponent,
+    HeaderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
@@ -31,12 +35,15 @@ import { NavigateComponent } from './components/navigate/navigate.component';
     MatFormFieldModule, 
     MatIconModule,
     MatInputModule,
-    MatToolbarModule
+    MatToolbarModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   exports: [
     MatTabsModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
